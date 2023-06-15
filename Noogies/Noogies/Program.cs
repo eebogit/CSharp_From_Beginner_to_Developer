9a51352f8b8d6c0ea78ea3c6b6c4f4b87a429c4b
D. Saylock <eebogit@users.noreply.github.com>
2023-06-15 13:37:40 -0400

    Noogies Creation, Upload
    Creation of Noogies directory, a directory dedicated to playing with various concepts of C# as well as my first smattering of a script.

---
+++ Noogies/Noogies/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Noogies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            int d;
+            int e;
+
+            Console.WriteLine("Pick a Number:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a Second Number:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a Larger Third Number:");
+            d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("=================");
+            c = a + b;
+            e = d - c;
+
+            while (c <= d)
+            {
+                Console.WriteLine($"Number {c} is {e} less than {d}. \n");
+                c++;
+                e--;
+            }
+        }
+    }
+}
