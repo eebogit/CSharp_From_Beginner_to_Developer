a1b0a3bf741cce059e09f87394b8228d8c831a70
D. Saylock <eebogit@users.noreply.github.com>
2023-06-17 14:00:48 -0400

    Update to Noogies- Created Do While Loop
    Rolled in [Do While] loop to allow for the script to be run multiple times.

---
+++ Noogies/Looping-Da-Loopa.cs
@@ -0,0 +1,7 @@
+﻿int intTre = 3;
+decimal decTreFur = 34.4M;
+string strName = "Bobby";
+
+strName = Bobby;
+Console.WriteLine($"Hello {strBob}");
+Console.WriteLine($"Hello {strBob}, you have {intTre} messages in your inbox. The outside temperature is {decTreFur}° Celcius.");
---
+++ Noogies/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Noogies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            int d;
+            int e;
+            string strYesNoAnswer = "";
+
+            do
+            {
+                Console.WriteLine("Pick a Number:");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a Second Number:");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a Larger Third Number:");
+                d = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("====================");
+                c = a + b;
+                e = d - c;
+
+                while (c <= d)
+                {
+                    Console.WriteLine($"Number {c} is {e} less than {d}. \n");
+                    c++;
+                    e--;
+                }
+                Console.WriteLine("Want to run again?");
+                strYesNoAnswer = Console.ReadLine().ToLower();
+                    while(!strYesNoAnswer.Equals("y") && !strYesNoAnswer.Equals("n")){
+                    Console.WriteLine("Not a valid choice, type \"y\" or \"n\" \n Please try again.");
+                    strYesNoAnswer = Console.ReadLine().ToLower();
+                }
+            }
+            while(strYesNoAnswer.Equals("y"));
+            // If the user answers no.
+            return;
+        }
+    }
+}
